bf1f8764eab90a81c53d9e37d56e7d8a02c266d7
Tiede Bakker <tiede.bakker@gmail.com>
2026-05-15 17:29:45 +0200

    update vocab app

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
   const [feedback, setFeedback] = useState("");
   const [correctCount, setCorrectCount] = useState(0);
 
+
   useEffect(() => {
     setWord(getRandomWord());
   }, []);
